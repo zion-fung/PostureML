853a3c36a9968b6a2cf910bebf5d6e3136efc0e1
Zion Fung <zfung1@stevens.edu>
2019-10-27 01:17:40 -0400

    Added new webpack app with persistent dashboard

---
+++ react/src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { Grid, Card, Image, Button, Segment } from 'semantic-ui-react';
+import "./App.css";
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        let startEvent = new Event("startTensor");
+        let stopEvent = new Event("stopTensor");
+        let resetEvent = new Event("setDefaultPose");
+        this.state = {
+            isRecording: false,
+            startEvent: startEvent,
+            stopEvent: stopEvent,
+            resetEvent: resetEvent
+        }
+    }
+    toggleRecord() {
+        let status = this.state.isRecording;
+        if (status) {
+            window.dispatchEvent(this.state.stopEvent);
+        } else {
+            window.dispatchEvent(this.state.startEvent);
+        }
+        this.setState({ isRecording: !status });
+    }
+    resetPose() {
+        window.dispatchEvent(this.state.resetEvent);
+    }
+    render() {
+        return (
+            <div className="parent">
+                <Grid columns={2} divided>
+                    <Grid.Column width={4} className="profile">
+                        <Card>
+                            <Card.Header>Profile</Card.Header>
+                            <Card.Meta>Zion Fung</Card.Meta>
+                            <Image src="logo192.png"></Image>
+                            <Card.Content>
+                                <Button
+                                    fluid
+                                    onClick={() => { this.toggleRecord() }}
+                                    color={this.state.isRecording ? "red" : "green"}>{this.state.isRecording ? "Stop" : "Start"}</Button>
+                                <Button
+                                    fluid
+                                    onClick={() => { this.resetPose() }}
+                                    color="grey">Reset Pose</Button>
+                            </Card.Content>
+                        </Card>
+                    </Grid.Column>
+                    <Grid.Column width={1}></Grid.Column>
+                    <Grid.Column width={11} className="content" stretched>
+                        <Grid.Row>
+                            <Segment>
+                                Graph here
+                            </Segment>
+                        </Grid.Row>
+                        <Grid.Row>
+                            <Segment className="shareOptions">
+                                <Button circular color='facebook' icon='facebook' />
+                                <Button circular color='twitter' icon='twitter' />
+                                <Button circular color='linkedin' icon='linkedin' />
+                                <Button circular color='google plus' icon='google plus' />
+                            </Segment>
+                        </Grid.Row>
+                    </Grid.Column>
+                </Grid>
+            </div>
+        );
+    }
+}
